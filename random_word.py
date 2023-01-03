40fb872c6e4d0b68f4646a718772e67bdc67a1a8
Sumit Das <sumitdas13082004@gmail.com>
2023-01-03 11:15:35 +0530

    Version 2

---
+++ random_word.py
@@ -0,0 +1,14 @@
+import os
+import random
+
+import random
+word=[]
+for x in range(10):
+    li=[chr(random.randint(97,122)) for a in range(random.randint(1,5))]
+    li=''.join(li)
+    word.append(li)
+print(word)
+input()
+os.system('cls')
+    
+        
